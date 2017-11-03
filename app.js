4a8fc1876f6fc69b3b5eddc9dbbcd8a6033f8e08
Chris Stuart <crstuart@gmail.com>
2017-11-03 14:00:46 -0400

    fixed screen name issue

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ app.set('view engine', 'pug');
 
 var friends = [];
 var tweets = [];
+var screen_name = [];
 
 
 //create new twitter application that generates keys and 
@@ -18,19 +19,33 @@ const Twit = require('twit');
 
 const user = new Twit(config);
 
-user.get('statuses/user_timeline', { screen_name: 'crsbos', count: 5},
+user.get('account/verify_credentials', { skip_status: true })
+  .catch(function (err) {
+    console.log('caught error', err.stack)
+  })
+  .then(function (result) {
+    // `result` is an Object with keys "data" and "resp". 
+    // `data` and `resp` are the same objects as the ones passed 
+    // to the callback. 
+    // See https://github.com/ttezel/twit#tgetpath-params-callback 
+    // for details. 
+ 
+    var screen_name = result.data.screen_name;
+  })
+
+user.get('statuses/user_timeline', { screen_name: screen_name, count: 5},
 	function(e, data, result) {
 	tweets = data;
 
 }); 
 
-user.get('friends/list', { screen_name: 'crsbos', count: 5},
+user.get('friends/list', { screen_name: screen_name, count: 5},
 	function(e, data, result) {
 	friends = data.users;
 
 }); 
 
-user.get('direct_messages', { screen_name: 'crsbos', count: 5},
+user.get('direct_messages', { screen_name: screen_name, count: 5},
 	function(e,data,result) {
 	dms = data;
 	console.log(dms);
